99957a6a06bed7f1758cb2e4bc1d2c728ea194a0
Jerome Marsall <jeromem@starberry.tv>
2022-03-14 15:01:43 +0530

    👌 IMPROVE: Content types

--- src/api/about-page/controllers/about-page.js
+++ src/api/about-page/controllers/about-page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  about controller
+ *  about-page controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::about.about');
+module.exports = createCoreController('api::about-page.about-page');
--- src/api/hero-page/routes/hero-page.js
+++ src/api/hero-page/routes/hero-page.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about router.
+ * hero-page router.
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::about.about');
+module.exports = createCoreRouter('api::hero-page.hero-page');
